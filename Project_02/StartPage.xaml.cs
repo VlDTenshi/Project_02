b85b39f5d857ac41bce7bf8a6096981ff9194ec4
VlDTenshi <118188335+VlDTenshi@users.noreply.github.com>
2024-04-01 17:09:42 +0300

    Q_01

--- Project_02/StartPage.xaml.cs
+++ Project_02/StartPage.xaml.cs
@@ -54,7 +54,7 @@ namespace Project_02
             // Обработчик нажатия на кнопку
             arrowButton.Clicked += async (sender, args) =>
             {
-                await Shell.Current.GoToAsync("//NavigatePage");
+                await Shell.Current.GoToAsync($"//{nameof(NavigatePage)}");
             };
 
             // Создаем макет для размещения элементов
